2aed09c7117656988e1b0409aea1bbca6fb54360
Louis Weiser <louis.weiser98@gmail.com>
2023-02-08 22:52:49 +0100

    naja

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const bodyElement = document.querySelector('[data-js="body"]');
+/* const bodyElement = document.querySelector('[data-js="body"]');
 
 let boolCounter = false;
 
@@ -10,4 +10,16 @@ toggleButton.addEventListener("click", () => {
   } else {
     bodyElement.classList.remove("dark");
   }
+}); */
+
+const cardOne = document.querySelector('[data-js="cardOne"]');
+const bookmark = document.querySelector('[data-js="svg-bookmark"]');
+
+cardOne.addEventListener("click", () => {
+  if (cardOne.checked === "true") {
+    bookmark.classList.add("fill");
+  }
+  console.log("event");
 });
+
+console.log(cardOne.checked);

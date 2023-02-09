4311dccbddc4d84c3db02fb45e79ec0638490815
Louis Weiser <louis.weiser98@gmail.com>
2023-02-09 11:47:11 +0100

    navbar-rework

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-/* const bodyElement = document.querySelector('[data-js="body"]');
-
-let boolCounter = false;
-
-const toggleButton = document.querySelector('[data-js="toggle-button"]');
-toggleButton.addEventListener("click", () => {
-  boolCounter = !boolCounter;
-  if (boolCounter) {
-    bodyElement.classList.add("dark");
-  } else {
-    bodyElement.classList.remove("dark");
-  }
-}); */
-
 const cardOne = document.querySelector('[data-js="cardOne"]');
 const bookmark = document.querySelector('[data-js="svg-bookmark"]');
 
